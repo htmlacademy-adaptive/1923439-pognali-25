bcd88865e7c1c975441b13b968fa4c407900a873
Вася Чехов <4exoff@novoeradio.by>
2022-04-28 09:02:07 +0300

    Сделал адаптивное модальное окно

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,10 @@
 let headerMenu = document.querySelector('.main-header');
 let headerMenuToggle = document.querySelector('.main-header__menu');
-
-
-
+let modalOpen = document.querySelector('.add-profile__link');
+let modalIndex = document.querySelector('.modal-container');
+let modalClose = document.querySelector('.modal-contant__link');
 
 headerMenu.classList.remove('main-header--nojs');
-
-
 
 headerMenuToggle.addEventListener('click', function () {
   if (headerMenu.classList.contains('main-header--close')) {
@@ -18,4 +16,17 @@ headerMenuToggle.addEventListener('click', function () {
   }
 });
 
+modalOpen.addEventListener('click', function (Event) {
 
+  Event.preventDefault();
+
+  if (modalIndex.classList.contains('modal-container--close')) {
+    modalIndex.classList.remove('modal-container--close');
+    modalIndex.classList.add('modal-container--open');
+  }
+});
+
+modalClose.addEventListener('click', function () {
+  modalIndex.classList.remove('modal-container--open');
+  modalIndex.classList.add('modal-container--close');
+});
